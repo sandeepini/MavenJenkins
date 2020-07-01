8b55abc414efd5644c10746d81bb2724b288e519
Shwetha <Shwetha@DESKTOP-EM8SP10>
2020-07-01 08:57:17 +0530

    First commit

---
+++ ActiTIME/src/test/java/com/actitime/test/CreateCustomer.java
@@ -0,0 +1,20 @@
+package com.actitime.test;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomer {
+	
+	@Test
+	public void testCreateCustomer() {
+		
+	Reporter.log("CreateCustomer",true);
+
+	}
+	@Test
+	public void testCreateCustomer2() {
+	Reporter.log("CreateCustomerwithDescription",true);
+	}
+
+}
